5852d6faf205464620d1bd8ecdfb7b490ac148b3
SanugondaVijayKumar <myemail>
2023-07-04 19:31:28 +0530

    output based on routes

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
+// const http=require('http');
+// const server=http.createServer((req,res)=>{
+//     res.writeHead(200,{'Content-Type':'text/plain'});
+//     const name="Vijay Kumar";
+//     console.log(name);
+//     res.end(name);
+
+// });
+
+// server.listen(4000,()=>{
+//     console.log("server is running on 4000");
+// })
+
 const http=require('http');
+// const server=http.createServer((req,res)=>{
+//     console.log(req.url,req.method,req.headers);
+//     //process.exit();
+//     res.setHeader('Content-Type','text/html');
+//     res.write('<html>');
+//     res.write('<head><title>My First Page</title></head>');
+//     res.write('<body><h1>Hello from my Node.js Server!</h1></body>')
+//     res.write('</html>');
+//     res.end();
+// });
+
 const server=http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    const name="Vijay Kumar";
-    console.log(name);
-    res.end(name);
+    const url=new URL(req.url,`http://${req.headers.host}`);
+    
 
-});
-
-server.listen(4000,()=>{
-    console.log("server is running on 4000");
+    if(url.pathname=='/home'){
+        res.writeHead(200,{'Content-Type':'text/plain'});
+        res.end('Welcome home');
+    }else if(url.pathname=='/about'){
+        res.writeHead(200,{'Content-Type':'text/plain'});
+        res.end('Welcome to About Us Page');
+    }else if(url.pathname=='/node'){
+        res.writeHead(200,{'Content-Type':'text/plain'});
+        res.end('Welcome to my Node.js Project');
+    }else{
+        res.writeHead(404,{'Content-Type':'text/plain'});
+        res.end('404 Not Found');
+    }
 })
+
+server.listen(3000);
--- play.js
+++ play.js
@@ -1,86 +1,23 @@
-// const name='Max';
-// let age=29;
-// const hasHobbies=true;
+const fetchData=()=>{
+    const promise=new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("Done!");
+        },1500);
+    });
 
-// age=30;
+    return promise;
+    
+};
 
-// const summerizeUser=(userName,userAge,userhasHobbies)=>{
-//     return ('Name is '+userName+', age is '+userAge+' and the user has hobbies: '+userhasHobbies);
-// }
+setTimeout(()=>{
+    console.log("Timer is done!");
+    fetchData().then(text=>{
+        console.log(text);
+        return fetchData()
+    }).then(text2=>{
+        console.log(text2);
+    })
+},2000);
 
-// const add=(a,b)=>{
-//     return a+b;
-// }
-
-
-// //Arrow function to return product of two numbers
-// const product=(a,b)=>{
-//     return a*b;
-// }
-
-// console.log(product(2,5));
-// console.log(add(1,2));
-// console.log(summerizeUser(name,age,hasHobbies));
-
-
-const person={
-    name:'Max',
-    age:29,
-    greet(){
-        console.log('Hi,I am '+this.name);
-    }
-}
-
-const copiedPerson={...person};
-console.log(copiedPerson);
-
-const printName=({name})=>{
-    console.log(name);
-}
-
-const {name,age}=person;
-console.log(name,age);
-// printName(person);
-
-// console.log(person);
-// person.greet();
-
-// const student={
-//     name:'vijay',
-//     age:'22'
-// }
-
-// console.log(student);
-
-hobbies=['Sports','cooking'];
-const [hobby1,hobby2]=hobbies;
-console.log(hobby1,hobby2);
-
-// const copiedArray=[...hobbies];
-// console.log(copiedArray);
-// for(let hobby of hobbies){
-//     console.log(hobby);
-// }
-
-// hobbies.push("programming");
-
-// console.log(hobbies.map((hobby)=>{
-//     return "Hobby: "+hobby;
-// }));
-// console.log(hobbies);
-
-// const arr= ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
-// const newarray=arr.map((item)=>{
-//     if(item==' '){
-//         return 'empty string';
-//     }else{
-//         return item;
-//     }
-// })
-
-// console.log(newarray);
-
-// const toArray=(...args)=>{
-//     return args;
-// }
-// console.log(toArray(5,-9,0));
+console.log("Hello");
+console.log("Hi!");
