61e6e6a2e05e6b229a1d86f6efbdfc63ce4f256f
SanugondaVijayKumar <myemail>
2023-07-04 12:15:16 +0530

    created new array

--- play.js
+++ play.js
@@ -1,26 +1,26 @@
-const name='Max';
-let age=29;
-const hasHobbies=true;
+// const name='Max';
+// let age=29;
+// const hasHobbies=true;
 
-age=30;
+// age=30;
 
-const summerizeUser=(userName,userAge,userhasHobbies)=>{
-    return ('Name is '+userName+', age is '+userAge+' and the user has hobbies: '+userhasHobbies);
-}
+// const summerizeUser=(userName,userAge,userhasHobbies)=>{
+//     return ('Name is '+userName+', age is '+userAge+' and the user has hobbies: '+userhasHobbies);
+// }
 
-const add=(a,b)=>{
-    return a+b;
-}
+// const add=(a,b)=>{
+//     return a+b;
+// }
 
 
-//Arrow function to return product of two numbers
-const product=(a,b)=>{
-    return a*b;
-}
+// //Arrow function to return product of two numbers
+// const product=(a,b)=>{
+//     return a*b;
+// }
 
-console.log(product(2,5));
-console.log(add(1,2));
-console.log(summerizeUser(name,age,hasHobbies));
+// console.log(product(2,5));
+// console.log(add(1,2));
+// console.log(summerizeUser(name,age,hasHobbies));
 
 
 const person={
@@ -31,12 +31,35 @@ const person={
     }
 }
 
-console.log(person);
-person.greet();
+// console.log(person);
+// person.greet();
 
-const student={
-    name:'vijay',
-    age:'22'
+// const student={
+//     name:'vijay',
+//     age:'22'
+// }
+
+// console.log(student);
+
+hobbies=['Sports','cooking'];
+for(let hobby of hobbies){
+    console.log(hobby);
 }
 
-console.log(student);
+hobbies.push("programming");
+
+console.log(hobbies.map((hobby)=>{
+    return "Hobby: "+hobby;
+}));
+// console.log(hobbies);
+
+// const arr= ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
+// const newarray=arr.map((item)=>{
+//     if(item==' '){
+//         return 'empty string';
+//     }else{
+//         return item;
+//     }
+// })
+
+// console.log(newarray);
